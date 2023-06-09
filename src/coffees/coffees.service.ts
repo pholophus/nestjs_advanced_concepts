2362f5fe09c3c3c483c917808ea6065581a530eb
pholophus <iqbalkisas6@gmail.com>
2023-06-09 11:02:42 +0800

    first commit

---
+++ src/coffees/coffees.service.ts
@@ -0,0 +1,35 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+import { Coffee } from './entities/coffee.entity';
+
+export const COFFEES_DATA_SOURCE = Symbol('COFFEES_DATA_SOURCE');
+
+export interface CoffeeDataSource {
+  [index:number]: Coffee;
+}
+
+@Injectable()
+export class CoffeesService {
+  constructor(@Inject(COFFEES_DATA_SOURCE) dataSource: CoffeeDataSource){}
+
+  create(createCoffeeDto: CreateCoffeeDto) {
+    return 'This action adds a new coffee';
+  }
+
+  findAll() {
+    return `This action returns all coffees`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} coffee`;
+  }
+
+  update(id: number, updateCoffeeDto: UpdateCoffeeDto) {
+    return `This action updates a #${id} coffee`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} coffee`;
+  }
+}
